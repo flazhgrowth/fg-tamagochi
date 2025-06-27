75d7c5ba465e0eeffc90563e07033f05ffa2d18e
Baeclatant <flazhgrowth@gmail.com>
2025-06-27 12:10:23 +0700

    fix(LogPath): change wrapError to LogError instead

--- pkg/http/apierrors/wrapper.go
+++ pkg/http/apierrors/wrapper.go
@@ -1,13 +1,25 @@
 package apierrors
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+)
 
 type LogPath string
 
+// With extends the LogPath value with given postfix
+/*
+	eg:
+		lw := LogPath("Lala")
+		lw.With("Lili") // lw value is "Lala.Lili"
+*/
 func (lw LogPath) With(postfix string) LogPath {
 	return LogPath(fmt.Sprintf("%s.%s", lw, postfix))
 }
 
-func (lw LogPath) WrapError(postfix string) error {
-	return fmt.Errorf("%s.%s", lw, postfix)
+// LogError logs error using zerolog log.Error.Msgf()
+func (lw LogPath) LogError(err error) {
+	lw = LogPath(fmt.Sprintf("%s:%s", lw, err.Error()))
+	log.Error().Msgf(string(lw))
 }
